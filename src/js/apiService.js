63a91a92d313574fdb71a473ed1a469b7074851e
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-10-09 13:26:56 +0300

    подключил библиотеку basicLightbox

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,23 +2,27 @@ import refs from "./refs.js"
 
 const apiKey = "18623369-889f6d1cb3a21a0bcc2be87ce";
 const baseUrl = `https://pixabay.com/api/`
-// let query = ""
-// let page = 1
-// let perPage = 12
 
 export default {
     _query: "",
     page: 1,
     perPage: 12,
 
-    fetchImage() {
+    async fetchImage() {
         let url = `${baseUrl}?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=${this.perPage}&key=${apiKey}`;
-        return fetch(url)
-            .then(res => res.json())
-            .catch(error => displayError(error))
+        try {
+            const res = await fetch(url);
+            const getResponse = await res.json();
+            return getResponse
+        } catch (error) {
+            throw error
+        }
     },
     setPage() {
         return this.page++;
+    },
+    resetPage() {
+        return this.page = 1;
     },
     get query() {
         return this._query;
--- src/js/renderApi.js
+++ src/js/renderApi.js
@@ -1,40 +1,55 @@
-import apiService from "./apiService.js"
-import refs from "./refs.js"
-import template from "../templates/template.hbs"
-import debounce from "lodash.debounce"
-// console.log(refs);
+import apiService from "./apiService.js";
+import refs from "./refs.js";
+import template from "../templates/template.hbs";
+import debounce from "lodash.debounce";
+import * as basicLightbox from 'basiclightbox';
+import "basiclightbox/dist/basicLightbox.min.css"
 
 const loadMoreButton = document.createElement("button")
 loadMoreButton.textContent = "Load More ..."
 loadMoreButton.classList.add("loadmore-button")
 
+refs.galleryList.addEventListener("click", (e) => {
+    if (e.target.nodeName === "IMG") {
+        const instance = basicLightbox.create(`
+        <div class="modal">
+        <img class="lightBoxImage" src="${e.target.dataset.src}" width="800" height="600">
+        </div>`);
+        instance.show()
+    }
+    // const imgModal = document.querySelector(".lightBoxImage")
+})
+
 refs.form.addEventListener("input", debounce(e => {
     e.preventDefault();
-    console.dir(e.target.value);
+    refs.galleryList.innerHTML = ""
+    apiService.resetPage()
     apiService.query = e.target.value
     rengeApi()
     refs.input.value = ""
-    // if (apiService.query.length >= 3) {
-    // }
-
 }, 1000),
-
 )
 
-
+loadMoreButton.addEventListener("click", loadMore)
 
 function rengeApi() {
     apiService.fetchImage().then(({ hits }) => rengeImage(hits))
     refs.body.insertAdjacentElement("beforeend", loadMoreButton)
-
 }
 function rengeImage(data) {
     const item = template(data)
     refs.galleryList.insertAdjacentHTML("beforeend", item)
 }
+function loadMore() {
+    apiService.setPage();
+    apiService.fetchImage().then(({ hits }) => {
+        rengeImage(hits)
+    })
+    setTimeout(() => {
+        window.scrollTo({
+            top: document.documentElement.offsetHeight - 1500,
+            behavior: "smooth"
+        })
+    }, 500);
+}
 
-
-// if (!refs.galleryList) {
-
-// } else {
-// }
